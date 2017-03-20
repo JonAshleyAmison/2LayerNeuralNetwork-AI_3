d875bc37f53036577f6a2acd044e65115b032f3f
JonAshleyAmison <jaamison55@gmail.com>
2017-03-20 15:43:43 -0500

    Lacks all of the learning. Parsing is done

--- src/Amison3.java
+++ src/Amison3.java
@@ -1,11 +1,11 @@
 
 import java.util.StringTokenizer;
 //test
+
 public class Amison3 {
 
     public static KeyboardInputClass input = new KeyboardInputClass();// access to KeyBoardInputClass()
     public static LList learnMe = new LList();
-    public static double[] weights = new double[121];
     public double learningRate = 0.01;
 
     public static void main(String[] args) {
@@ -23,21 +23,20 @@ public class Amison3 {
                 newTextFileOut.getFileNamePassed(textFile.text[i + 1]);
                 newTextFileIn.getFileContents();
                 newTextFileOut.getFileContents();
-                Object newObject = new inOut(newTextFileIn.text[0], newTextFileOut.text[0]);
+                double[] weights = new double[121];
+                for (int t = 0; t < 121; t++) {
+                    double random = Math.random() - .5;
+                    System.out.println(random);
+                    weights[t] = random;
+                }
+                Object newObject = new inOut(newTextFileIn.text[0], newTextFileOut.text[0], weights);
                 i++;
                 learnMe.add(newObject);
-            }
-            for (int i = 0; i < 121; i++) {
-                double random = Math.random()-.5;
-                System.out.println(random);
-                weights[i] = random;
             }
             char quitMe = input.getCharacter(true, 'N', "Y,N", 1, "Would You Like To Exit The Program? (Y/N Default=N)");
             if (quitMe == 'Y') {                                        // exit the program
                 break;
-            }
-            else{
-                weights = new double[121];
+            } else {
                 learnMe.clear();
             }
         }
@@ -50,15 +49,23 @@ public class Amison3 {
     public static void trainer() {
 
     }
+
+    public static double delta(int target, int input, double weight) {
+        double delta = target - (/*Ei??**/input * weight);
+
+        return delta;
+    }
 }
 
 class inOut {
 
     public String in;
     public String desired;
+    public double[] weights;
 
-    public inOut(String passedIn, String passedDesired) {
+    public inOut(String passedIn, String passedDesired, double[] passedWeights) {
         in = passedIn;
         desired = passedDesired;
+        weights = passedWeights;
     }
 }
