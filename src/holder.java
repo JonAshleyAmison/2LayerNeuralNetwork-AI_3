6e639369d8411257e7c85b69b7dd2ce9ebe3f1c1
JonAshleyAmison <jaamison55@gmail.com>
2017-03-20 15:27:31 -0500

    Test1

---
+++ src/holder.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jonashleyamison
+ */
+public class holder {
+//prints out the arrays
+//    if (parsed.equals("-1")) {
+//                i++;
+//                System.out.print(X);
+//            }
+//            if (parsed.equals("1")) {
+//                i++;
+//                System.out.print(space);
+//            }
+//            if(i==11||i==22||i==33||i==44||i==55||i==66||i==77||i==88||i==99||i==110||i==121){
+//                System.out.print("\n");
+//            }
+//********************
+//********************
+
+}
